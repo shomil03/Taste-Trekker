acd94345305725502d2950fa4da7f2a4f944effc
shomil03 <shomil02020@gmail.com>
2023-12-11 15:52:05 +0530

    Initial Commit

---
+++ Taste-Trekker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taste-Trekker
+//
+//  Created by Shomil Singh on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taste-Trekker/Taste_TrekkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taste_TrekkerApp.swift
+//  Taste-Trekker
+//
+//  Created by Shomil Singh on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Taste_TrekkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
